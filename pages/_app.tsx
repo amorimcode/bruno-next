6b9a04e0b2cbdc6a58c39a89267aa5d02f7ecd14
amorimcode <bruno.amorim032@gmail.com>
2022-12-26 20:24:58 -0300

    home start

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
+import { ThemeProvider } from "next-themes";
+import "../styles/globals.css";
+
+const interVariable = Inter({ display: "swap" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <main className={interVariable.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }
